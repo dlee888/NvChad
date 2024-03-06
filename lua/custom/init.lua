63832a29cdd2e9f8b318d72606cd440d7d70726b
jeffarjeffar <jeffarjeffar@gmail.com>
2024-03-06 14:00:40 -0500

    clipboard

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -10,3 +10,15 @@ vim.cmd "autocmd BufNewFile,BufRead *.bsv set ft=bsv"
 vim.cmd "autocmd BufNewFile,BufRead *.ms set ft=bsv"
 vim.cmd "autocmd FileType bsv set shiftwidth=4 expandtab smarttab"
 
+vim.cmd [[let g:clipboard = {
+                \   'name': 'WslClipboard',
+                \   'copy': {
+                \      '+': 'clip.exe',
+                \      '*': 'clip.exe',
+                \    },
+                \   'paste': {
+                \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                \   },
+                \   'cache_enabled': 0,
+                \ }]]
