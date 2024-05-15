90083f095e62d456561d4e7a07a363de2d7ce8ed
dlee888 <dlee888@mit.edu>
2024-05-15 08:15:58 -0400

    remove jank wsl

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -10,19 +10,6 @@ vim.cmd "autocmd BufNewFile,BufRead *.bsv set ft=bsv"
 vim.cmd "autocmd BufNewFile,BufRead *.ms set ft=bsv"
 vim.cmd "autocmd FileType bsv set shiftwidth=4 expandtab smarttab"
 
-vim.cmd [[let g:clipboard = {
-                \   'name': 'WslClipboard',
-                \   'copy': {
-                \      '+': 'clip.exe',
-                \      '*': 'clip.exe',
-                \    },
-                \   'paste': {
-                \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-                \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-                \   },
-                \   'cache_enabled': 0,
-                \ }]]
-
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = { "*.c", "*.h", "*.cpp", "*.hpp", "*.cc", "*.hh", "*.c++", "*.h++", "*.cxx", "*.hxx" },
   callback = function()
